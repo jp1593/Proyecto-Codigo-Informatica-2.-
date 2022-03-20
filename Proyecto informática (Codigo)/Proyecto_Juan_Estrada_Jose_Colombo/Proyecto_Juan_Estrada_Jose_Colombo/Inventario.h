a9547956d8123f6b2d00b66ba9902f6d96f7de16
jp1593 <juanpablo08082002@hotmail.com>
2022-03-19 19:27:15 -0600

    Funcionalidades ingreso y visualizar libro

---
+++ Proyecto informática (Codigo)/Proyecto_Juan_Estrada_Jose_Colombo/Proyecto_Juan_Estrada_Jose_Colombo/Inventario.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+
+class Inventario
+{
+private: 
+	std::string nombre; 
+	std::string autor;
+	std::string fecha;
+	std::string edicion;
+	std::string estado;
+	std::string destino;
+	std::string editorial;
+	std::string categoria; 
+
+public: 
+
+	int libroingresado[5]; 
+
+	void setNombre(std::string n)
+	{
+		this->nombre = n; 
+	}
+
+	void setAutor(std::string a)
+	{
+		this->autor = a; 
+	}
+
+	void setFecha(std::string ñ)
+	{
+		this->fecha = ñ; 
+	}
+
+	void setEdicion(std::string e)
+	{
+		this->edicion = e; 
+	}
+
+	void setEstado(std::string s)
+	{
+		this->estado = s; 
+	}
+
+	void setDestino(std::string d)
+	{
+		this->destino = d;
+	}
+
+	void setCategoria(std::string c)
+	{
+		this->categoria = c; 
+	}
+
+	void setEditorial(std::string edd)
+	{
+		this->editorial = edd; 
+	}
+
+
+	std::string getNombre()
+	{
+		return this->nombre; 
+	}
+
+	std::string getAutor()
+	{
+		return this->autor; 
+	}
+
+	std::string getFecha()
+	{
+		return this->fecha; 
+	}
+
+	std::string getEdicion()
+	{
+		return this->edicion; 
+	}
+
+	std::string getEstado()
+	{
+		return this->estado; 
+	}
+
+	std::string getDestino()
+	{
+		return this->destino; 
+	}
+
+	std::string getCategoria()
+	{
+		return this->categoria; 
+	}
+
+	std::string getEditorial()
+	{
+		return this->editorial; 
+	}
+
+};
+
